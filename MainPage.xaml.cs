b932a15fa9406157070e9d69c13af37cfda5e491
Aleksander Holthe <aleholthe@gmail.com>
2024-10-20 20:54:04 +0200

    removed historical data button on main page

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -7,8 +7,7 @@
             InitializeComponent();
         }
         private async void OnLiveSensorData1ButtonClicked(object sender, EventArgs e) => await Navigation.PushAsync(new LiveSensorData());
-        private async void OnLiveWheaterData1ButtonClicked(object sender, EventArgs e) => await Navigation.PushAsync(new LiveWheaterData());
-        private async void OnHistData1ButtonClicked(object sender, EventArgs e) => await Navigation.PushAsync(new HistoricalData_1("sensorname"));
+        private async void OnLiveWeatherData1ButtonClicked(object sender, EventArgs e) => await Navigation.PushAsync(new LiveWheaterData());
     }
 }
 
